341e657abfec5421cbcf06b1b146d5da864566cc
Mike HUdecheck <mhudecheck@gmail.com>
2021-11-29 13:52:48 +0000

    Changes to sortSAFE()

--- src/Sentinel.jl
+++ src/Sentinel.jl
@@ -699,7 +699,7 @@ module Sentinel
     end
 
     # Takes a list of SAFE directories, groups them by geometries, and returns them in sorted order (most -> lest recent)
-    function sortSAFE(tile, directory) 
+    #=function sortSAFE(tile, directory) 
         path = generateSAFEPath(tile, directory)
         safeList = readdir(path)
         safeDF = DataFrame([safeList], :auto)
@@ -726,6 +726,62 @@ module Sentinel
             end
         end
         return groupA, groupB
+    end=#
+
+    function sortSAFE(tile, directory) 
+        path = generateSAFEPath(tile, directory)
+        safeList = readdir(path)
+        safeDF = DataFrame([safeList], :auto)
+        safeDF[!, :sort] = SubString.(safeList, 12, 19)
+        sort!(safeDF, :sort; rev=true)
+        safeList = safeDF[!, 1]
+        fileAGeom, fileANoData = extractSAFEGeometries(path * safeList[1]);
+        areaList = []
+        if fileANoData < 1
+            safeDF[!, :group] .= 1  
+        else 
+            geomPtr = LibGEOS._readgeom(fileAGeom)
+            push!(areaList, LibGEOS.geomArea(geomPtr))
+            for i in 2:length(safeList)
+                fileBGeom, fileBNoData = extractSAFEGeometries(path * safeList[i])
+                geomPtr = LibGEOS._readgeom(fileBGeom)
+                push!(areaList, LibGEOS.geomArea(geomPtr))
+            end
+            zed = kde(Vector{Float64}(areaList), npoints=4, boundary=(minimum(areaList),maximum(areaList)))
+            group = []
+            @show zed.x[1], zed.x[2], zed.x[3], zed.x[4]
+            for i in areaList
+                if i <= zed.x[2]
+                    a = 1
+                elseif zed.x[2] < i <= zed.x[3]
+                    a = 2
+                else
+                    a = 3
+                end
+                push!(group, a)
+            end
+            safeDF[!, :group] = group
+            safeDF[!, :area] = areaList
+            grouped_df  = groupby(safeDF, "group")
+            safeDF2 = combine(grouped_df, x -> nrow(x) < 3 ? DataFrame() : x)
+            #nGroups = unique(y[!, :group])
+            nGroups = combine(groupby(z, [:group]), nrow => :count)
+            nGroups = sort(nGroups, :count)
+            if nrow(nGroups) > 1
+                for j in 1:2
+                    g = nGroups[j, :group]
+                    if j == 1
+                        a = subset(safeDF2, :group => (x -> x .== (g)))
+                    elseif j == 2
+                        b = subset(safeDF2, :group => (x -> x .== (g)))
+                    end
+                end
+            else 
+                a = subset(safeDF2, :group => (x -> x .== (g)))
+                b = similar(a, 0)
+            end
+        end
+        return a[!, :x1], b[!, :x1]
     end
 
     # QC Function (% of no data obs) 
